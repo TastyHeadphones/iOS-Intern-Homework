f81d335c5544334c31dc7b294b80cd78a1fdc938
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2023-02-02 10:35:44 +0800

    add load more spinner

--- iTunes-Music/InfraUI/SpinnerCell.swift
+++ iTunes-Music/InfraUI/SpinnerCell.swift
@@ -5,4 +5,29 @@
 //  Created by Magic Keegan on 2/1/23.
 //
 
-import Foundation
+import IGListKit
+
+class SpinnerCell: UICollectionViewCell {
+
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView()
+        view.style = UIActivityIndicatorView.defaultStyle
+        self.contentView.addSubview(view)
+        return view
+    }()
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let bounds = contentView.bounds
+        activityIndicator.center = CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+}
+
+extension UIActivityIndicatorView {
+    class var defaultStyle: UIActivityIndicatorView.Style {
+        if #available(iOS 13.0, *) {
+            return .medium
+        } else {
+            return .gray
+        }
+    }
+}
