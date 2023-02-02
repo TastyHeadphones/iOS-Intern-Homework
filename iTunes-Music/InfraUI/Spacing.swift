d1f1204c80b8a330ebca4bf8cc82d83a526e6a59
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2023-02-02 15:20:19 +0800

    [UI] Optimize SongSearchCell looks better (#3)
    ![Simulator Screen Shot - iPhone 14 - 2023-02-02 at 15 17
    52](https://user-images.githubusercontent.com/76083688/216257718-5c5abfcd-a152-4282-b125-94b7b210bc9e.png)

--- iTunes-Music/InfraUI/Spacing.swift
+++ iTunes-Music/InfraUI/Spacing.swift
@@ -6,10 +6,12 @@
 //
 
 struct Spacing {
-    static let point = 1
-    static let small = 5
-    static let xsmall = 8
-    static let medium = 10
-    static let xmedium = 15
-    static let large = 20
+    static let point = 1.0
+    static let small = 5.0
+    static let xsmall = 8.0
+    static let medium = 10.0
+    static let xmedium = 15.0
+    static let large = 20.0
+    static let imageHeight = 60.0
+    static let cellHeight = 80.0
 }
--- iTunes-Music/iTunes-Music/SongSearch/SongSearchCell.swift
+++ iTunes-Music/iTunes-Music/SongSearch/SongSearchCell.swift
@@ -12,10 +12,30 @@ import IGListKit
 class SongSearchCell: UICollectionViewCell {
 
     let containerView = UIView()
-    let imageView = UIImageView()
-    let collectionNameLabel = UILabel()
-    let artistNameLabel = UILabel()
-    let trackNameLabel = UILabel()
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = Spacing.small
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    let collectionNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray
+        label.font = label.font.withSize(Spacing.xmedium)
+        return label
+    }()
+    let artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray
+        label.font = label.font.withSize(Spacing.xmedium)
+        return label
+    }()
+    let trackNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = label.font.withSize(Spacing.xmedium)
+        return label
+    }()
     let separator: CALayer = {
         let layer = CALayer()
         layer.backgroundColor = UIColor.defaultSeparator.cgColor
@@ -25,24 +45,26 @@ class SongSearchCell: UICollectionViewCell {
     func setupConstraints() {
         containerView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(Spacing.xmedium)
-            make.top.bottom.equalToSuperview().inset(Spacing.xsmall)
+            make.top.bottom.equalToSuperview().inset(Spacing.small)
         }
         imageView.snp.makeConstraints { make in
             make.leading.equalToSuperview().inset(Spacing.medium)
-            make.top.bottom.equalToSuperview().inset(Spacing.xsmall)
-            make.width.equalTo(self.contentView.snp.height)
-        }
-        collectionNameLabel.snp.makeConstraints { make in
-            make.leading.equalTo(imageView.snp.trailing).offset(Spacing.medium)
-            make.height.equalToSuperview()
-        }
-        artistNameLabel.snp.makeConstraints { make in
-            make.leading.equalTo(collectionNameLabel.snp.trailing).offset(Spacing.medium)
-            make.height.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.height.width.equalTo(Spacing.imageHeight)
         }
         trackNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView)
+            make.leading.equalTo(imageView.snp.trailing).offset(Spacing.medium)
+            make.trailing.equalToSuperview().inset(Spacing.medium)
+        }
+        artistNameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(imageView.snp.trailing).offset(Spacing.medium)
+            make.top.equalTo(trackNameLabel.snp.bottom).offset(Spacing.medium)
+        }
+        collectionNameLabel.snp.makeConstraints { make in
             make.leading.equalTo(artistNameLabel.snp.trailing).offset(Spacing.medium)
-            make.height.equalToSuperview()
+            make.trailing.equalToSuperview().inset(Spacing.medium)
+            make.top.equalTo(trackNameLabel.snp.bottom).offset(Spacing.medium)
         }
         separator.frame = CGRect(x: 0, y: containerView.bounds.height, width: containerView.bounds.width, height: CGFloat(Spacing.point))
     }
