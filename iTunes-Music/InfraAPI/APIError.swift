7a3b0f4c355db5f60244133caae19c00e3e5e48c
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2023-02-04 20:43:35 +0800

    add exception

--- iTunes-Music/InfraAPI/APIError.swift
+++ iTunes-Music/InfraAPI/APIError.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+enum APIError: Error, LocalizedError {
+    case unknown, apiError(reason: String), parserError(reason: String), networkError(from: URLError)
+
+    var errorDescription: String? {
+        switch self {
+        case .unknown:
+            return "Unknown error"
+        case .apiError(let reason), .parserError(let reason):
+            return reason
+        case .networkError(let from):
+            return from.localizedDescription
+        }
+    }
+}
