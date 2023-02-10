8bd78a9a365340bf698838f01185b46801b5c712
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2023-02-10 14:05:18 +0800

    add a11y and modify tests specs

--- iTunes-Music/iTunes-MusicTests/LayoutTests/SongDetailViewControllerLayoutTests.swift
+++ iTunes-Music/iTunes-MusicTests/LayoutTests/SongDetailViewControllerLayoutTests.swift
@@ -11,18 +11,17 @@ import LayoutTest
 class SongDetailViewControllerLayoutTests: LayoutTestCase {
     func testLayout() {
         runLayoutTests(withViewProvider: SongDetailViewController.self) { _, _, context in
-            accessibilityTestsEnabled = false
             guard let detailVC = context as? SongDetailViewController else {
                 XCTAssert(false)
                 return
             }
-            detailVC.imageView.lyt_above(detailVC.trackNameLabel)
-            detailVC.trackNameLabel.lyt_above(detailVC.collectionNameLabel)
-            detailVC.collectionNameLabel.lyt_above(detailVC.artistNameLabel)
-            detailVC.artistNameLabel.lyt_above(detailVC.releaseDateNameLabel)
-            detailVC.releaseDateNameLabel.lyt_above(detailVC.playButton)
-            detailVC.artistPreviewButton.lyt_before(detailVC.playButton)
-            detailVC.collectionPreviewButton.lyt_after(detailVC.playButton)
+            XCTAssertTrue(detailVC.imageView.lyt_above(detailVC.trackNameLabel))
+            XCTAssertTrue(detailVC.trackNameLabel.lyt_above(detailVC.collectionNameLabel))
+            XCTAssertTrue(detailVC.collectionNameLabel.lyt_above(detailVC.artistNameLabel))
+            XCTAssertTrue(detailVC.artistNameLabel.lyt_above(detailVC.releaseDateNameLabel))
+            XCTAssertTrue(detailVC.releaseDateNameLabel.lyt_above(detailVC.playButton))
+            XCTAssertTrue(detailVC.artistPreviewButton.lyt_before(detailVC.playButton))
+            XCTAssertTrue(detailVC.collectionPreviewButton.lyt_after(detailVC.playButton))
         }
     }
 }
--- iTunes-Music/iTunes-MusicTests/LayoutTests/SongSearchCellLayoutTests.swift
+++ iTunes-Music/iTunes-MusicTests/LayoutTests/SongSearchCellLayoutTests.swift
@@ -11,17 +11,16 @@ import LayoutTest
 class SongSearchCellLayoutTests: LayoutTestCase {
     func testLayout() {
         runLayoutTests(withViewProvider: SongSearchCell.self) { view, _, _ in
-            accessibilityTestsEnabled = false
             guard let view = view as? SongSearchCell else {
                 XCTAssert(false)
                 return
             }
-            view.imageView.lyt_before(view.trackNameLabel)
-            view.imageView.lyt_before(view.collectionNameLabel)
-            view.imageView.lyt_before(view.artistNameLabel)
-            view.trackNameLabel.lyt_above(view.artistNameLabel)
-            view.trackNameLabel.lyt_above(view.collectionNameLabel)
-            view.artistNameLabel.lyt_before(view.collectionNameLabel)
+            XCTAssertTrue(view.imageView.lyt_before(view.trackNameLabel))
+            XCTAssertTrue(view.imageView.lyt_before(view.collectionNameLabel))
+            XCTAssertTrue(view.imageView.lyt_before(view.artistNameLabel))
+            XCTAssertTrue(view.trackNameLabel.lyt_above(view.artistNameLabel))
+            XCTAssertTrue(view.trackNameLabel.lyt_above(view.collectionNameLabel))
+            XCTAssertTrue(view.artistNameLabel.lyt_before(view.collectionNameLabel))
         }
     }
 }
